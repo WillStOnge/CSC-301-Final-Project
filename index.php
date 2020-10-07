760d4b54b13d7c63bc9f2ba36037f1abf270eccd
Will St. Onge <ongewill@gmail.com>
2020-10-06 22:19:42 -0400

    Completed certification object

--- index.php
+++ index.php
@@ -11,20 +11,20 @@
 
 <body class="dark-mode">
     <div class="container">
-        <?php
-        session_start();
+        <div class="text-right pt-5">
+            <?php
+            session_start();
 
-        if (isset($_SESSION['user_id']))
-            echo '<div class="text-right pt-5">
-                    <small class="align-text-top">Welcome back, ' . str_replace("%\n%", '', $_SESSION['user_name']) . '</small>
-                    <a href="logout.php"><button type="button" class="btn btn-sm">Logout</button></a>
-                  </div>';
-        else
-            echo '<div class="text-right pt-5">
-                    <a href="login.php"><button type="button" class="btn btn-sm">Login</button></a>
-                    <a href="register.php"><button type="button" class="btn btn-sm">Register</button></a>
-                  </div>';
-        ?>
+            if (isset($_SESSION['user_id']))
+                echo '<small class="align-text-top">Welcome back, ' . str_replace("%\n%", '', $_SESSION['user_name']) . '</small>
+                    <a href="logout.php"><button type="button" class="btn btn-sm">Logout</button></a>';
+            else
+                echo '<a href="login.php"><button type="button" class="btn btn-sm">Login</button></a>
+                    <a href="register.php"><button type="button" class="btn btn-sm">Register</button></a>';
+            ?>
+        </div>
+        <a href="search.php"><button type="button" class="btn btn-sm">Search for contractors</button></a>
+        <a href="worker.php?id=10"><button type="button" class="btn btn-sm">Test worker page</button></a>
     </div>
 </body>
 
--- objects/certification.php
+++ objects/certification.php
@@ -5,27 +5,109 @@ class Certification extends IObject
 {
     public $certification_id;
     public $worker_id;
-    public $file_path;
+    public $file_name;
     public $validated;
 
-    static function create()
+    static function create($worker_id, $file_name, $validated)
     {
+        $certification = new Certification();
 
+        $certification_id = 0;
+        try
+        {
+            $certification->conn->beginTransaction();
+            $stmt = $certification->conn->prepare("INSERT INTO certification (worker_id, file_name, validated) VALUES (:worker_id, :file_name, :validated)");
+            $stmt->bindParam(':worker_id', $worker_id, PDO::PARAM_INT);
+            $stmt->bindParam(':file_name', $file_name);
+            $stmt->bindParam(':validated', $validated);
+            $stmt->execute();
+
+            $certification_id = $certification->conn->lastInsertId(); 
+
+            $certification->conn->commit();
+        }
+        catch (Exception $e)
+        {
+            $certification->conn->rollBack();
+            http_response_code(500);
+            die('Error 500');
+        }
+
+        return $certification->read($certification_id);
     }
 
-    static function read()
+    static function read($certification_id)
     {
+        $record = NULL;
+        $certification = new Certification();
 
+        try
+        {
+            $certification->conn->beginTransaction();
+            $stmt = $certification->conn->prepare("SELECT * FROM certification WHERE certification_id = :certification_id");
+            $stmt->bindParam(":certification_id", $certification_id, PDO::PARAM_INT);
+            $stmt->execute();
+            $record = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if ($record === false)
+                throw new Exception("Record not found.");
+
+            $certification->conn->commit();
+        }
+        catch (Exception $e)
+        {
+            $certification->conn->rollBack();
+            http_response_code(500);
+            die('Error 500');
+        }
+
+        $certification->certification_id = $record["certification_id"];
+        $certification->worker_id = $record["worker_id"];
+        $certification->file_name = $record["file_name"];
+        $certification->validated = $record["validated"];
+
+        return $certification;
     }
 
     function update()
     {
+        try
+        {
+            $this->conn->beginTransaction();
+            $stmt = $this->conn->prepare("UPDATE certification SET worker_id = :worker_id, file_name = :file_name, validated = :validated WHERE certification_id = :certification_id");
+            $stmt->bindParam(':certification_id', $this->certification_id, PDO::PARAM_INT);
+            $stmt->bindParam(':worker_id', $this->worker_id, PDO::PARAM_INT);
+            $stmt->bindParam(':file_name', $this->file_name);
+            $stmt->bindParam(':validated', $this->validated);
+            $stmt->execute();
 
+            $this->conn->commit();
+        }
+        catch (Exception $e)
+        {
+            $this->conn->rollBack();
+            http_response_code(500);
+            die('Error 500');
+        }
     }
 
     function delete()
     {
-        
+        try
+        {
+            $this->conn->beginTransaction();
+            $stmt = $this->conn->prepare("DELETE FROM certification WHERE certification_id = :certification_id");
+            $stmt->bindParam(':certification_id', $this->certification_id, PDO::PARAM_INT);
+            $stmt->execute();
+
+            $this->conn->commit();
+        }
+        catch (Exception $e)
+        {
+            $this->conn->rollBack();
+            http_response_code(500);
+            die('Error 500');
+        }
     }
 }
 ?>
