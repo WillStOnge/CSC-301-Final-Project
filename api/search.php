3e63dc51c6a609de533519d707c11fac5317a452
Will St. Onge <ongewill@gmail.com>
2020-10-30 16:05:57 -0400

    Added search function to home page

---
+++ api/search.php
@@ -0,0 +1,66 @@
+<?php
+// TODO: Test this on the home page.
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+include_once "../objects/utils/database.php";
+
+session_start();
+
+if (!isset($_GET['query']))
+{
+    http_response_code(400);
+    die(json_encode(["message" => "No query specified"]));
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "GET") 
+{
+    $queries = explode(" ", $_GET['query']);
+    $in = "'" . implode("','", $queries) . "'";
+
+    $db = new Database();
+    $workers;
+    $workerData = array();
+
+    $query = "SELECT DISTINCT worker.*, user.*, rating FROM worker NATURAL JOIN skill NATURAL JOIN user LEFT OUTER JOIN (SELECT worker_id, AVG(star_rating) AS rating FROM review GROUP BY worker_id) reviews ON worker.worker_id = reviews.worker_id WHERE skill_name IN (" . $in . ") AND status = 'APPROVED' AND banned = false ORDER BY rating DESC";
+
+    try
+    {
+        $db->conn->beginTransaction();
+        $stmt = $db->conn->prepare($query);
+        $stmt->execute();
+        $workers = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        if ($workers === false)
+            die('[]');
+
+        $db->conn->commit();
+    }
+    catch (Exception $e)
+    {
+        $db->conn->rollBack();
+        http_response_code(500);
+        die('Error 500' . $e);
+    }
+
+    foreach ($workers as &$worker)
+    {
+        $description = substr($worker["description"], 0, 150);
+
+        $description = strlen($description) < 150 ? $description : rtrim($description) . "...";
+
+        array_push($workerData, array(
+            "worker_id" => $worker["worker_id"],
+            "description" => $description,
+            "location" => $worker["location"],
+            "name" => $worker["name"],
+            "rating" => $worker["rating"])
+        );
+    }
+    
+    echo json_encode($workerData);
+}
+?>
--- index.php
+++ index.php
@@ -6,6 +6,51 @@
     <link href="https://cdn.jsdelivr.net/npm/halfmoon@1.1.0/css/halfmoon-variables.min.css" rel="stylesheet" />
     <script src="https://cdn.jsdelivr.net/npm/halfmoon@1.1.0/js/halfmoon.min.js"></script>
     <title>Home page</title>
+    <script>
+    function search() {
+        var request = new XMLHttpRequest();
+
+        var query = document.getElementById('query').value
+        var outer = document.getElementById('workers')
+
+        outer.innerHTML = ''
+
+        request.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                var response = JSON.parse(this.response)
+
+                response.forEach((worker) => {
+                    var card = document.createElement('div')
+                    var title = document.createElement('h3')
+                    var link = document.createElement('a')
+                    var loc = document.createElement('h6')
+                    var desc = document.createElement('p')
+
+                    card.setAttribute('class', 'col-sm-5 card')
+                    title.setAttribute('class', 'card-title')
+                    link.setAttribute('class', 'hyperlink text-white')
+                    link.setAttribute('href', 'worker.php?id=' + worker.worker_id)
+                    desc.setAttribute('class', 'text-muted')
+
+                    link.innerText = worker.name + ' - '
+                    loc.innerText = worker.location
+                    desc.innerText = worker.description
+
+                    for (var i = 0; i < worker.rating; i++)
+                        link.innerHTML += "&#11088;"
+
+                    outer.appendChild(card)
+                    card.appendChild(title)
+                    title.appendChild(link)
+                    card.appendChild(loc)
+                    card.appendChild(desc)
+                })
+            }
+        };
+        request.open("GET", "api/search.php?query=" + query, true);
+        request.send();
+    }
+    </script>
 </head>
 
 <body class="dark-mode">
@@ -14,7 +59,7 @@
             <?php
             session_start();
 
-            echo '<div class="row"><div class="col-sm-6 align-text-top pt-5">';
+            echo '<div class="row"><div class="col-sm-6 align-text-top text-left pt-5">';
 
             if (isset($_SESSION['is_admin']) && $_SESSION['is_admin'])
                 echo '<a href="admin.php"><button type="button" class="btn btn-sm">Admin Page</button></a>';
@@ -27,18 +72,30 @@
                         <a href="logout.php"><button type="button" class="btn btn-sm">Logout</button></a>
                     </div>';
             else
-            {
                 echo '<div class="col-sm-6 text-right pt-5">
                         <a href="login.php"><button type="button" class="btn btn-sm">Login</button></a>
                         <a href="register.php"><button type="button" class="btn btn-sm">Register</button></a>
                     </div>';
-            }
 
             echo '</div>';
             ?>
         </div>
-        <a href="worker.php?id=10"><button type="button" class="btn btn-sm">Test worker page</button></a>
-        <a href="skills.php"><button type="button" class="btn btn-sm">Skills page</button></a>
+
+        <div class="card">
+            <h3 class="card-title text-center">Search for Workers</h3>
+            <div class="row">
+                <div class="col-sm">
+                    <input type="text" class="form-control" placeholder="Enter skills here" id='query'>
+                </div>
+                <div class="col-sm pl-5">
+                    <button onclick="search()" class="btn">Submit</button>
+                </div>
+            </div>
+        </div>
+        
+        <div class="row" id="workers">
+            
+        </div>
     </div>
 </body>
 
